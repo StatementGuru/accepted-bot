f69ae068811f12398c3872b235c60048f677a2be
StatementGuru <contact@statementguru.com>
2026-05-08 03:51:57 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
